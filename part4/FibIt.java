7290ff5dbfeb334e216703e2d96d616c4806bc30
Thomas Tran <thomastran878@gmail.com>
2018-02-14 01:43:31 -0800

    Part4 fib working

--- part4/FibIt.java
+++ part4/FibIt.java
@@ -1,16 +1,37 @@
 public class FibIt implements  SeqIt {
-    private Fib fib1;
+    int cur1 = 0;
+    int cur2 = 0;
+    int last = 0;
+
+    int count = 0;
+
     public FibIt(Fib inFib) {
-        this.fib1 = inFib;
+        cur1 = inFib.firstOne;
+        cur2 = inFib.firstTwo;
+        last = inFib.last1;
     }
 
     public int next() {
-
-        return 0;
+        if (count == 0) {
+            count++;
+            return cur1;
+        }
+        else if (count == 1) {
+            count++;
+            return cur2;
+        }
+        int newCur = cur1 + cur2;
+        cur1 = cur2;
+        cur2 = newCur;
+        return newCur;
     }
 
     public boolean hasNext() {
-
-        return false;
+        if (cur1 == last && count == 0)
+            return true;
+        if (cur1 + cur2 <= last)
+            return true;
+        else
+            return false;
     }
 }
